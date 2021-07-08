b7e3a19fb290e10f964ff435707e71a9e1930496
DevJ5 <idevelopjs@gmail.com>
2021-07-08 17:51:13 +0200

    Cleanup

--- project/src/store/utility.js
+++
@@ -1,6 +0,0 @@
-export const updateObject = (oldObject, newValue) => {
-  return {
-    ...oldObject,
-    ...newValue,
-  };
-};
--- routing--assignment-problem/src/Components/NoMatch/NoMatch.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NoMatch = () => {
-  return <h1>Post not found</h1>;
-};
-
-export default NoMatch;
